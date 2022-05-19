e7c68fa9eb28564080cab66e3d1d089197e9a563
davi-moura <davimoura55@gmail.com>
2022-05-19 18:53:40 -0300

    commit

--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,14 @@
 <h3>sobrenos (view)</h3>
+
+
+<ul>
+    <li>
+        <a href="{{ route('site.index') }}">Principal</a>
+    </li>
+    <li>
+        <a href="{{ route('site.sobrenos') }}">Sobre nos</a>
+    </li>
+    <li>
+        <a href="{{ route('site.contato') }}">Contato</a>
+    </li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobre-nos', 'SobreNosController@sobrenos');
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+
+Route::get('/contato', 'ContatoController@contato')->name('site.contato'); 
+
+Route::get('/login', function(){return 'Login';})->name('site.login'); 
+
+
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes',     function(){return 'clientes';})->name('site.clientes'); 
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('site.fornecedores'); 
+    Route::get('/produtos',     function(){return 'produtos';})->name('site.produtos'); 
+});
